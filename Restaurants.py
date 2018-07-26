9291f7c873f2ee435d96d82a67195a63a101fcae
ElifK <elifkarasoglu@gmail.com>
2018-07-26 17:09:59 +0300

    adding Restaurant features

---
+++ Restaurants.py
@@ -0,0 +1,17 @@
+class Restaurants():
+
+    def __init__(self):
+
+        self.name = None
+        self.coordinates = None
+        self.openinghours = None
+        self.freeSeats = None
+
+    def print_information(self):
+
+#        list = [
+
+         print ("der Name von Restaurant : ",self.name,'\t', "Koordinaten : ",self.coordinates,'\t', "Oefnungszeiten : ",
+                self.openinghours,'\t', "Anzahl von frei Plaetze : ",self.freeSeats)
+
+
