6f409a98c31e3dccb0231249079dbbd1a0f1296f
Your Name <caner.arslan96@hotmail.com>
2018-07-27 16:22:06 +0200

    adding more person features

--- allUnittests.py
+++ allUnittests.py
@@ -7,18 +7,14 @@ class TestGreetingMethods(unittest.TestCase):
 
     def test_offically(self):
 
-        user2=Person()
-        user2.first_name="Erdem"
-        user2.second_name="Cimenoglu"
+        user2=Person(first_name="Erdem", second_name="Cimenoglu")
         greet_value = user2.greet()
 
         self.assertEqual(greet_value[0:9], "Sehr geeh")
 
     def test_inoffically(self):
 
-        user3=Person()
-        user3.first_name="Enes"
-        user3.second_name="Akay"
+        user3=Person(first_name="Enes", second_name="Akay")
         user3.official =False
 
         greet_value = user3.greet()
